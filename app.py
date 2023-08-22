2ee067c391111c7917c29eff75ee1a9d5bbae4cf
Dhruv <200120116042@git.org.in>
2023-08-22 16:46:57 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+import pickle
+from flask import Flask, render_template, redirect, request, jsonify ,url_for
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
+## import ridge model and stadndard scaler pickel file
+linear_model = pickle.load(open("models/linear_regression.pkl","rb"))
+Standard_scaler = pickle.load(open("models/scaler.pkl","rb"))
+
+## route from home page
+
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def index():
+    return render_template("index.html")
+
+@app.route("/my_data", methods = ['GET','POST'])
+def predict_datapoint():
+    if request.method == "POST":
+        TOEFL_Score = float(request.form.get("TOEFL_Score"))
+        CGPA = float(request.form.get("CGPA"))
+        SOP = float(request.form.get("SOP"))
+        LOR = float(request.form.get("LOR"))
+        University_Rating = float(request.form.get("University_Rating"))
+        GRE_Score = float(request.form.get("GRE_Score"))
+        Research = float(request.form.get("Research"))
+
+        new_data = Standard_scaler.transform([[TOEFL_Score,CGPA,SOP,LOR,University_Rating,GRE_Score,Research]])
+        resultt = linear_model.predict(new_data)
+
+        return render_template("index.html",result=resultt[0]*100)
+
+    else :  
+        return render_template("index.html")
+
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0" ,debug=True)
